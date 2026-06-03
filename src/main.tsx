27178941073d2284304eec14a2fc7d85f4f54b8b
riquetti95 <adrianoriquetti@gmail.com>
2026-06-03 13:52:59 -0300

    Ajuste no cache para versionamento

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ensureAppVersion, setupVersionWatcher } from "./lib/app-version-guard";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ensureAppVersion();
+setupVersionWatcher();
+
+createRoot(document.getElementById("root")!).render(<App />);
